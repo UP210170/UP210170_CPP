59f74a78aedb3a45b57ff2220452d43d0f519319
UP210775 <up210775@alumnos.upa.edu.mx>
2022-10-07 09:39:46 -0500

    Unidad 2

--- U2/07_BinToDEc.cpp
+++ U2/07_BinToDEc.cpp
@@ -1,6 +1,6 @@
 /*
 Date: 05/10/2022
-Date last modification: 05/10/2022
+Date last modification: 08/10/2022
 Author: Oscar Noel Nuño Verdín
 Email: up210170@alumnos.upa.edu.mx
 Description: Exercise Unit 2
@@ -13,23 +13,36 @@ Description: Exercise Unit 2
 using namespace std;
 
 int main (){
-    int bin;
+    int deci;
+    string resultado;
  
+    cout << "Programa de conversión de Decimal a Binario" << endl;
+    cout << "Ingresa el número decimal: ";
+    cin >> deci;
 
-    cout << "Ingresa el número binario: ";
-    cin >> bin;
-    
-    while(bin > 0){
-
-    }
-
-
-    cout << "El número decimal es: ";
-    
+    do
+    {
+        if (deci > 0)
+        {
+            while(deci != 0){
+                if(deci % 2 == 0){
+                    resultado = "0" + resultado;
+                }else{
+                    resultado = "1" + resultado;
+                }
+                deci /= 2;
+            }
+         cout << "El número binario es: " << resultado << endl;
+        }
+        else if (deci == 0)
+        {
+            cout << "0" << endl;
+        }
+        else
+        {
+            cout << "Debe ser mayor a cero" << endl;
+        }
+    }while(deci < 0);
 
     return 0;
-
-
-
-    ///Codigo en proceso 
 }
