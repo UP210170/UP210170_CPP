52ea5f888dbb5685c42eabe35dd56df95b40e5a4
UP210775 <up210775@alumnos.upa.edu.mx>
2022-10-12 10:19:54 -0500

    Unidad 2

--- U2/08_TablaMulti.cpp
+++ U2/08_TablaMulti.cpp
@@ -8,14 +8,38 @@ int main (){
     int numero, i;
     int limite;
 
-    cout << "Ingresa un limite para la tabla: ";
+    cout << "Enter a table limit: ";
     cin >> limite;
 
-    cout << "Ingrese un número entero: ";
+    cout << "Enter a whole number: ";
     cin >> numero;
 
-    for (i = 1; i <= limite; i++){
-        cout << numero << "x" << i << "=" << numero * i;
+
+    cout << "The number table" << numero << endl;
+
+
+
+    if (numero <= 1 || numero >= 1)
+    {
+        for (i = 1; i <= limite; i++)
+        {
+            if (i == 1)
+            {
+                for (int espacio = 0; espacio <= 39; espacio++)
+                {
+                    cout << "-";
+                }
+                cout << "\n";
+            }
+            
+            cout << "|" << i << "\t x \t" << numero << "\t = \t" << (numero * i) << "\t|\n";
+
+            for (int espacio = 0; espacio <= 39; espacio++)
+            {
+                cout << "-";
+            }
+            cout << "\n";
+        }
     }
 
     return 0;
