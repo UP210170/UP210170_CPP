493686fa3fea2d6a34c92e729fcf3910c9a389fd
UP210775 <up210775@alumnos.upa.edu.mx>
2022-10-24 10:38:55 -0500

    Unidad 3

---
+++ U3/01_Cambio.cpp
@@ -0,0 +1,98 @@
+ /*
+Date: 24/09/2022
+Author: Oscar Noel Nuño Verdín
+Email: up210170@alumnos.upa.edu.mx
+Description: Unit 3 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void obtenerCambio(int);
+void imprimirCambio(string);
+
+
+int quinientos = 0, docientos = 0, cien = 0;
+int cincuenta = 0, veinte = 0;
+int diez = 0, cinco = 0, dos = 0, uno = 0; 
+
+int main(){
+    int dinero;
+
+    cout << "Ingresa una cantidad de dinero: ";
+    cin >> dinero;
+
+    obtenerCambio(dinero);
+    imprimirCambio("TOTAL");
+
+
+   return 0;
+}
+
+void obtenerCambio(int dinero){
+    while (dinero > 0)
+    {
+        (dinero >= 500) ? quinientos++, dinero -= 500 
+                        : (dinero >= 200) ? docientos++, dinero -= 200 
+                        : (dinero >= 100) ? cien++, dinero -= 100 
+                        : (dinero >= 50) ? cincuenta++, dinero -= 50 
+                        : (dinero >= 20) ? veinte++, dinero -= 20 
+                        : (dinero >= 10) ? diez++, dinero -= 10 
+                        : (dinero >= 5) ? cinco++, dinero -= 5 
+                        : (dinero >= 2) ? dos++, dinero -= 2 
+                        : (dinero >= 1) ? uno++, dinero -= 1 
+                        : dinero; 
+        /*
+        if(dinero >= 500){
+            dinero = dinero - 500;
+            quinientos++;
+        }
+        else if(dinero >= 200){
+            dinero = dinero - 200;
+            docientos++;
+        }
+        else if(dinero >= 100){
+            dinero = dinero - 100;
+            cien++;
+        }
+        else if(dinero >= 50){
+            dinero = dinero - 50;
+            cincuenta++;
+        }
+        else if(dinero >= 20){
+            dinero = dinero - 20;
+            veinte++;
+        }
+        else if(dinero >= 10){
+            dinero -= 10;
+            diez++;
+        }
+        else if(dinero >= 5){
+            dinero -= 5;
+            cinco++;
+        }
+        else if(dinero >= 2){
+            dinero -= 2;
+            dos++;
+        }
+        else if(dinero >= 1){
+            dinero -= 1;
+            uno++;
+        }
+    }
+*/
+    }
+
+void imprimirCambio(string dinero){
+    cout << "EL cambio de dinero ingresado: " << dinero << endl;
+    cout << "Billetes de $500: " << quinientos << endl;
+    cout << "Billetes de $200: " << docientos << endl;
+    cout << "Billetes de $100: " << cien << endl;
+    cout << "Billetes de $50: " << cincuenta << endl;
+    cout << "Billetes de $20: " << veinte << endl;
+    cout << "Monedas de 10: " << diez << endl;
+    cout << "Monedas de 5: " << cinco << endl;
+    cout << "Monedas de 2: " << dos << endl;
+    cout << "Monedas de 1: " << uno << endl;
+}
