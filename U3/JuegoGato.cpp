20ca325b2f673ad7f1b7c2c27ffd8ae29be5b60a
UP210775 <up210775@alumnos.upa.edu.mx>
2022-11-03 10:37:36 -0600

    Unidad 3

--- U3/JuegoGato.cpp
+++ U3/JuegoGato.cpp
@@ -11,32 +11,40 @@ using namespace std;
 
 
 void hacerestructura();
-void seleccionarJugada();
+int seleccionarJugada();
 void convertirJugada(int jugada);
 bool comprobarCasillaOcupada(int jugada);
 
 
 char estructuraGato[3][3];
-int fila;
-int columna;
+int row = 0;
+int col = 0;
+int jugada;
+
+
 int main(){
-    int jugada;
-    bool casillaOcupada = true;
-    estructuraGato[0][0];
+    /*
+    estructuraGato[0][0] = 'q';
+    estructuraGato[0][1] = 'w';
+    estructuraGato[0][2] = 'e';
+    estructuraGato[1][0] = 'r';
+    estructuraGato[1][1] = 't';
+    estructuraGato[1][2] = 'y';
+    estructuraGato[2][0] = 'u';
     
-    do{
-        jugada = seleccionarJugada();
-        casillaOcupada = comprobarCasillaOcupada(jugada);
-        if(casillaOcupada == true){
-            cout << "Otra Vez";
-        }
-    }while(casillaOcupada == true)
+    */
+   estructuraGato[2][1] = 'i';
+    estructuraGato[2][2] = 'o';
+
+    hacerestructura();
+    seleccionarJugada();
     
-   
+    
+    
     return 0;
 }
 
-void hacerestructura (){
+void hacerestructura(){
     for(int fila = 0; fila < 6; fila++){
         for(int columna = 0; columna < 3; columna++){
 
@@ -44,7 +52,8 @@ void hacerestructura (){
                 cout << "___";
             }else{
                 if(fila < 5){
-                    cout << " " << estructuraGato[0][0] << " ";
+                    cout << " " << estructuraGato[row][col] << " ";
+                    col++;
                 }else{
                     cout << "   ";
                 }
@@ -54,54 +63,64 @@ void hacerestructura (){
                 cout << "|";
             }
         }
+        col = 0;
+        if(fila % 2 == 0){
+            row++;
+        }
         cout << endl;
     }
 }
 
 int seleccionarJugada(){
-    int jugada;
-    do{
-        cout << "Dame tu jugada";
-        cin >> jugada;
-    }while(jugada <= 9 && jugada > 0);
+    cout << "Elige una de estas jugadas: " << endl;
+    for(int fila = 0; fila < 3; fila++){
+        for(int columna = 0; columna < 3; columna++){
+            cout << "[" << fila << ", " << columna << "]" << endl;
+            if(estructuraGato[fila][columna] != NULL){
+                cout << "OCUPADO";
+            }
+            cout << endl;
+        }
+    }
 
-    return jugada;
+
+
 }
 
 void convertirJugada(int jugada){
     if(jugada == 1){
-        fila = 0;
-        columna = 0;
+        row = 0;
+        col = 0;
     }else if(jugada == 2){
-        fila = 0;
-        columna = 1;
+        row = 0;
+        col = 1;
     }else if(jugada == 3){
-        fila = 0;
-        columna = 2;
+        row = 0;
+        col = 2;
     }else if(jugada == 4){
-        fila = 1;
-        columna = 0;
+        row = 1;
+        col = 0;
     }else if(jugada == 5){
-        fila = 1;
-        columna = 1;
+        row = 1;
+        col = 1;
     }else if(jugada == 6){
-        fila = 1;
-        columna = 2;
+        row = 1;
+        col = 2;
     }else if(jugada == 7){
-        fila = 2;
-        columna = 0;
+        row = 2;
+        col = 0;
     }else if(jugada == 8){
-        fila = 2;
-        columna = 1;
+        row = 2;
+        col = 1;
     }else if(jugada == 9){
-        fila = 2;
-        columna = 2;
+        row = 2;
+        col = 2;
     }
 }
 
 
 bool comprobarCasillaOcupada(int jugada){
-    if(estructuraGato[fila][columna] == 'X' || estructuraGato[fila][columna] == 'O'){
+    if(estructuraGato[row][col] == 'X' || estructuraGato[row][col] == 'O'){
         return true;
     }else{
         return false;
