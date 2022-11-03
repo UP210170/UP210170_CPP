b44cf5881e09aebd8be6f3636b95fcaebf23f96d
UP210775 <up210775@alumnos.upa.edu.mx>
2022-11-03 09:53:23 -0600

    Unidad 3

--- U3/JuegoGato.cpp
+++ U3/JuegoGato.cpp
@@ -11,11 +11,27 @@ using namespace std;
 
 
 void hacerestructura();
-char estructuraGato[3][3];
+void seleccionarJugada();
+void convertirJugada(int jugada);
+bool comprobarCasillaOcupada(int jugada);
 
+
+char estructuraGato[3][3];
+int fila;
+int columna;
 int main(){
-    estructuraGato[0][0] = 'X';
-    hacerestructura();
+    int jugada;
+    bool casillaOcupada = true;
+    estructuraGato[0][0];
+    
+    do{
+        jugada = seleccionarJugada();
+        casillaOcupada = comprobarCasillaOcupada(jugada);
+        if(casillaOcupada == true){
+            cout << "Otra Vez";
+        }
+    }while(casillaOcupada == true)
+    
    
     return 0;
 }
@@ -40,5 +56,54 @@ void hacerestructura (){
         }
         cout << endl;
     }
+}
 
+int seleccionarJugada(){
+    int jugada;
+    do{
+        cout << "Dame tu jugada";
+        cin >> jugada;
+    }while(jugada <= 9 && jugada > 0);
+
+    return jugada;
+}
+
+void convertirJugada(int jugada){
+    if(jugada == 1){
+        fila = 0;
+        columna = 0;
+    }else if(jugada == 2){
+        fila = 0;
+        columna = 1;
+    }else if(jugada == 3){
+        fila = 0;
+        columna = 2;
+    }else if(jugada == 4){
+        fila = 1;
+        columna = 0;
+    }else if(jugada == 5){
+        fila = 1;
+        columna = 1;
+    }else if(jugada == 6){
+        fila = 1;
+        columna = 2;
+    }else if(jugada == 7){
+        fila = 2;
+        columna = 0;
+    }else if(jugada == 8){
+        fila = 2;
+        columna = 1;
+    }else if(jugada == 9){
+        fila = 2;
+        columna = 2;
+    }
+}
+
+
+bool comprobarCasillaOcupada(int jugada){
+    if(estructuraGato[fila][columna] == 'X' || estructuraGato[fila][columna] == 'O'){
+        return true;
+    }else{
+        return false;
+    }
 }
